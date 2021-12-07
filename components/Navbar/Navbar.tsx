81a2059e289c78b260a71536fd9bced3e896e08f
Thiago Vieira <th91vi@gmail.com>
2021-12-07 13:14:20 -0300

    feat: added logic to Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import Link from 'next/link';
 
 export const Navbar = () => {
-  return <div></div>;
+  const user = null;
+  const userName = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/" passHref>
+            <button type="button" className="btn-logo">
+              FEED
+            </button>
+          </Link>
+        </li>
+        {userName && (
+          <>
+            <li className="push-left">
+              <Link href="/admin" passHref>
+                <button type="button" className="btn-blue">
+                  Write Posts
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${userName}`} passHref>
+                <img src={user?.photoUrl} alt={userName} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {!userName && (
+          <li>
+            <Link href="/enter" passHref>
+              <button type="button" className="btn-blue">
+                Log in
+              </button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 };
