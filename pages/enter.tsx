e41bfbe3d50e8d9f3a9626d77e697b616fad5a80
Thiago Vieira <th91vi@gmail.com>
2021-12-07 13:48:36 -0300

    feat: added Google sign in method boilerplate

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+const SignInButton = () => {
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <button type="button" className="btn-google" onClick={signInWithGoogle}>
+      <img src="/google-logo.png" alt="Google logo" /> Sign in with Google
+    </button>
+  );
+};
+
+const SignOutButton = () => (
+  <button type="button" onClick={() => auth.signOut()}>
+    Sign out
+  </button>
+);
+const UsernameForm = () => {
+  return null;
+};
 
 const EnterPage = () => {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h1>Sign up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 };
